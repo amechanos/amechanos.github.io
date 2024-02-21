8a5e6a34949d9cd13c9ef92d9b9f2fddc0999463
jhxu <88032718+amechanos@users.noreply.github.com>
2024-02-21 21:25:43 +1100

    New Project

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onscroll = function() {
     // Adjust the background position based on the scroll position
     var scrollPosition = window.scrollY;
-    document.querySelector('.bg').style.backgroundPositionY = (-scrollPosition * 1.75) + 'px';
+    document.querySelector('.bg').style.backgroundPositionY = (-scrollPosition * 1.5) + 'px';
 };
