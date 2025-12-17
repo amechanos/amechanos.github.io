398ee79ca0193fd1b0e2d3ecaa861326e739041d
jhxu <88032718+amechanos@users.noreply.github.com>
2025-12-17 16:01:19 +1100

    Image Location

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,31 +1,36 @@
 import "../styles/social.css";
+import YouTubeIcon from './assets/youtube.svg';
+import SpotifyIcon from './assets/spotify.svg';
+import LinkedInIcon from './assets/linkedin.svg';
+import InstagramIcon from './assets/instagram.svg';
+import DiscordIcon from './assets/discord.svg';
 
 export default function SocialLinks({ className = '' }: { className?: string }) {
   return (
     <ul className={"social-links " + className}>
         <li>
             <a href="https://discord.gg/E46X78Wd8k" aria-label="Discord" title="Discord" target="_blank" rel="noopener noreferrer">
-                <img src="/src/assets/discord.svg" alt="" />
+                <img src={DiscordIcon} alt="" />
             </a>
         </li>
         <li>
             <a href="https://instagram.com/jhxu_07" aria-label="Instagram" title="Instagram" target="_blank" rel="noopener noreferrer">
-                <img src="/src/assets/instagram.svg" alt="" />
+                <img src={InstagramIcon} alt="" />
             </a>
         </li>
         <li>
             <a href="https://linkedin.com/in/jhxu07" aria-label="LinkedIn" title="LinkedIn" target="_blank" rel="noopener noreferrer">
-                <img src="/src/assets/linkedin.svg" alt="" />
+                <img src={LinkedInIcon} alt="" />
             </a>
         </li>
         <li>
             <a href="https://open.spotify.com/artist/69YiNb7bGO98SmJ7qL0Jh2?" aria-label="Spotify" title="Spotify" target="_blank" rel="noopener noreferrer">
-                <img src="/src/assets/spotify.svg" alt="" />
+                <img src={SpotifyIcon} alt="" />
             </a>
         </li>
         <li>
             <a href="https://youtube.com/@jhxu" aria-label="YouTube" title="YouTube" target="_blank" rel="noopener noreferrer">
-                <img src="/src/assets/youtube.svg" alt="" />
+                <img src={YouTubeIcon} alt="" />
             </a>
         </li>
     </ul>
