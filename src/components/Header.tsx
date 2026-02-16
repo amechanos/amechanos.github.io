5aaf98687a7c8ec5996c6f59f4039ee312967560
jhxu <88032718+amechanos@users.noreply.github.com>
2026-02-16 22:22:45 +1100

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import "../styles/header.css";
-import SocialLinks from './SocialLinks'
 import logo from '../assets/logo.svg'; 
 
 export default function Header() {
@@ -63,7 +62,7 @@ export default function Header() {
           <a href="#about" className={active === '#about' ? 'active' : ''} onClick={() => handleNavClick('#about')}>About</a>
           <a href="#contact" className={`cta ${active === '#contact' ? 'active' : ''}`} onClick={() => handleNavClick('#contact')}>Contact</a>
         </nav>
-        
+
       </div>
     </header>
   );
